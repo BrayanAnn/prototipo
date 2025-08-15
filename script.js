6dee869c46911841fb12c182b7b47b21d971a3f0
Brayan <brayan-anderson@outlook.com>
2025-08-15 15:27:07 -0300

    feat: initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const ordens = [
+    { id: 1, classificacao: "TAPA BURACO", descricao: "Buraco em via pública", status: "NOVO", criacao: "06/07/25", inspecao: "", inicio: "", termino: "", conclusao: "" },
+    { id: 2, classificacao: "MANUTENCAO DE HIDRANTES", descricao: "Hidrante quebrado", status: "EM ANDAMENTO", criacao: "06/07/25", inspecao: "07/07/25", inicio: "08/07/25", termino: "", conclusao: "" },
+    { id: 3, classificacao: "TAPA BURACO", descricao: "Tapa Buraco", status: "NOVO", criacao: "06/07/25", inspecao: "", inicio: "", termino: "", conclusao: "" },
+  ];
